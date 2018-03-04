8eee6935ef017792f8312fcb1352136fd56130ca
nekokan <nekokan@nekocompany.com>
2018-03-04 03:07:22 +0000

    user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # ルート/にアクセスしたら
+  # toppagesコントロールのindexアクションが動いて
+  # 表示はtoppages/index.html.erbだよ
+  root to: 'toppages#index'
+  
+  
+  
+  # ルート/signupにアクセスしたら
+  # usersコントロールのnewアクションが動いて
+  # 表示はusers/new.html.erbだよ
+  get 'signup', to: 'users#new'
+  resources :users, only: [:show, :new, :create]
+  
 end
