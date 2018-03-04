bc3523d4547786f03711db5943e910dddba8b157
nekokan <nekokan@nekocompany.com>
2018-03-04 06:51:56 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # 表示はtoppages/index.html.erbだよ
   root to: 'toppages#index'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   
   # ルート/signupにアクセスしたら
