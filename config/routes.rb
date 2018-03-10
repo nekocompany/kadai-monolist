d830ea127df5dbf068f4a03d9ac48febbf4212c0
nekokan <nekokan@nekocompany.com>
2018-03-10 03:30:24 +0000

    want

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,8 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users, only: [:show, :new, :create]
   
+  
+  resources :items, only: [:show, :new]
+  resources :ownerships, only: [:create, :destroy]
+  
 end
